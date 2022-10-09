689947cdc5836ad32872a2a81735a8c0ec58cb22
riya <riya.r123@gmail.com>
2022-10-09 19:16:11 +0530

    FirstCommit

---
+++ AngularQuizApp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
+import { ChangeBgDirective } from './change-bg.directive';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent,
+    ChangeBgDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
